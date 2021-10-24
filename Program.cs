8d773fd31f557d412979c3f5132ec4e4d7e63ab8
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-24 17:51:42 -0300

    resolução exercício

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  numero;
+            
+            Console.Write ("Digite um número:");
+            numero= Convert.ToInt32 (Console.ReadLine());
+
+            Console.Write ($"{ numero } é ");
+
+            if (numero  %  2  ==  0)
+            Console.WriteLine ("par");
+            else
+            Console.WriteLine ("ímpar");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+        }
+    }
+}
